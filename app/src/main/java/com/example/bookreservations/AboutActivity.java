8379500023a9b1216f9e3d03b132915cb17e7226
Andrea Waltlova <andywaltlova@gmail.com>
2020-09-17 11:25:21 +0200

    Added text to about activity

--- app/src/main/java/com/example/bookreservations/AboutActivity.java
+++ app/src/main/java/com/example/bookreservations/AboutActivity.java
@@ -5,8 +5,9 @@ import androidx.core.app.NavUtils;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-//TODO: Pridat popis
+
 public class AboutActivity extends AppCompatActivity {
 
     @Override
@@ -14,6 +15,16 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
+
+        String text = "Application is used to monitor requests for books that are in stocks of Central Library of the Faculty of Arts at Masaryk University.\n" +
+                "\n" +
+                "When a new request for a book appears, the application sends a notification containing the location number of the book, which makes it easier to find it in the library stocks.\n" +
+                "\n" +
+                "The purpose of this application is primarily to streamline work. Now there is no need to walk around the printer and check if a new request is printed. As a library, we guarantee 20 minutes to process each book request, so it's important for staff to find out about new requests as soon as possible, regardless of where they are in the library.\n" +
+                "\n" +
+                "The image below illustrates the API response that is used to retrieve current book reservations. For our purposes, the application uses the time, date, barcode and location number fields.";
+        TextView about_text = findViewById(R.id.about_text);
+        about_text.setText(text);
     }
 
 
